1741a797ea089a313d0a32cb7425b4b9a2067281
slolow <laszlostarost@googlemail.com>
2023-02-01 02:43:11 +0100

    update: cleaner code for myNameIs func

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,21 +1,17 @@
 /* delay function
 source: https://alvarotrigo.com/blog/wait-1-second-javascript/  */
-const delay = milliseconds => new Promise(resolve => setTimeout(resolve, milliseconds));
+const delay = (milliseconds) =>
+  new Promise((resolve) => setTimeout(resolve, milliseconds));
 
 /* show fullname when mouseover name */
 const myNameIs = async () => {
-    const nameSpan = document.getElementsByClassName('name')[0];
-    nameSpan.innerHTML = 'What?';
+  const nameSpan = document.getElementsByClassName("name")[0];
+  lyrics = ["What?", "Who?", "Chicka-chicka", "Laszlo Starost."];
+  for (lyric of lyrics) {
+    nameSpan.innerHTML = lyric;
     await delay(1000);
-    nameSpan.innerHTML = 'Who?';
-    await delay(1000);
-    nameSpan.innerHTML = 'Chika-chika';
-    await delay(1000);
-    nameSpan.innerHTML = 'Laszlo Starost.';
-    await delay(1000);
-}
+  }
+};
 
-const nameP = document.getElementsByClassName('name-paragraph')[0];
-nameP.addEventListener('mouseover', myNameIs, {once:true});
-
-
+const nameP = document.getElementsByClassName("name-paragraph")[0];
+nameP.addEventListener("mouseover", myNameIs, { once: true });
