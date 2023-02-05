c5e33a4a0dec764571d79bfa2cf7d34a81d918c2
slolow <laszlostarost@googlemail.com>
2023-02-05 22:34:20 +0100

    Update: Added about-me section

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,9 +1,12 @@
+/* universal */
 /* delay function
 source: https://alvarotrigo.com/blog/wait-1-second-javascript/  */
 const delay = (milliseconds) =>
   new Promise((resolve) => setTimeout(resolve, milliseconds));
 
-/* show fullname when mouseover name */
+
+/* banner */
+/* show fullname in banner when mouseover name */
 const myNameIs = async () => {
   const nameSpan = document.getElementsByClassName("name")[0];
   lyrics = ["What?", "Who?", "Chicka-chicka", "Laszlo Starost."];
@@ -15,3 +18,21 @@ const myNameIs = async () => {
 
 const nameP = document.getElementsByClassName("name-paragraph")[0];
 nameP.addEventListener("mouseover", myNameIs, { once: true });
+
+/* about-me */
+/*  show more-about-me or hide it */
+const showMoreOrLessAboutMe = event => {
+  if (event.target.innerHTML === 'Show more') {
+    document.getElementById('more-about-me').style.display = 'block';
+    event.target.innerHTML = 'Show less';
+  } 
+  else {
+    document.getElementById('more-about-me').style.display = 'none';
+    event.target.innerHTML = 'Show more';
+    document.getElementById('about-me').scrollIntoView();
+
+  }
+}
+
+const moreAboutMeBtn = document.getElementById('more-about-me-button');
+moreAboutMeBtn.addEventListener('click', showMoreOrLessAboutMe)
